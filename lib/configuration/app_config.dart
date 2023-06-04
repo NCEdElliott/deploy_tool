a8229f566b81f0b4b60109100b76d2cbf7cc7e6f
Ed Elliott <ed.elliott@pearson.com>
2023-06-04 16:24:55 -0400

    Creating AppConfig class

--- lib/configuration/app_config.dart
+++ lib/configuration/app_config.dart
@@ -1,3 +1,29 @@
 class AppConfig {
-  
+  static String getMacOSShell() {
+    return "/bin/bash";
+  }
+
+  static String getWindowsShell() {
+    return "C:\\Windows\\System32\\cmd.exe";
+  }
+
+  static String getLinuxShell() {
+    return "/bin/bash";
+  }
+
+  static String getMacOSSfdxCommand() {
+    return "/usr/local/bin/sf";
+  }
+
+  static String getWindowsSfdxCommand() {
+    return "C:\\Program Files\\nodejs\\sf";
+  }
+
+  static String getMacOSWorkingDir() {
+    return "/Users/eddie/Dev/Pearson/SFDX2";
+  }
+
+  static String getWindowsWorkingDir() {
+    return "C:\\Users\\edell\\Dev\\Pearson\\SFDX2";
+  }
 }
--- lib/helpers/sf_helper.dart
+++ lib/helpers/sf_helper.dart
@@ -1,4 +1,7 @@
-import 'package:flutter/material.dart';
+import '../configuration/app_config.dart';
 
 class SFHelper {
+  static String getInitials(String name) {
+    return AppConfig.getMacOSSfdxCommand();
+  }
 }
