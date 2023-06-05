37d08b1291aa1906acb12e69a0ffadc10029f2d7
Ed Elliott <ed.elliott@pearson.com>
2023-06-04 21:18:13 -0400

    Latest

--- lib/helpers/shellHelper.dart
+++ lib/helpers/shellHelper.dart
@@ -4,12 +4,12 @@ import 'package:deploy_tool/configuration/appConfig.dart';
 class shellHelper {
   static String retrieveMetadata() {
     String output = "";
-    Directory workingDir = Directory("${AppConfig.getMacOSWorkingDir()}/TestProject");
+    Directory workingDir =
+        Directory("${AppConfig.getMacOSWorkingDir()}/TestProject");
 
-    Process.run('ls', ['-l'], 
-                runInShell: true,
-                workingDirectory: workingDir.path.toString())
-            .then((ProcessResult results) {
+    Process.run('ls', ['-l'],
+            runInShell: true, workingDirectory: workingDir.path.toString())
+        .then((ProcessResult results) {
       print('Current Working Dir: ${workingDir.path}');
       print('Exit Code: ${results.exitCode}');
       print('Output: ${results.stdout}');
