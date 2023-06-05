50293c5e0fe77e5b2f660bb656e6d65be0574918
Ed Elliott <ed.elliott@pearson.com>
2023-06-05 12:48:31 -0400

    cleaned up print output

--- lib/helpers/shellHelper.dart
+++ lib/helpers/shellHelper.dart
@@ -1,22 +1,29 @@
 import 'dart:io';
+import 'package:process_run/shell.dart';
 import 'package:deploy_tool/configuration/appConfig.dart';
 
 class shellHelper {
-  static String retrieveMetadata() {
+  static Future<String> retrieveMetadata() async {
     String output = "";
     Directory workingDir =
         Directory("${AppConfig.getMacOSWorkingDir()}/TestProject");
 
-    Process.run('ls', ['-l'],
-            runInShell: true, workingDirectory: workingDir.path.toString())
-        .then((ProcessResult results) {
-      print('Current Working Dir: ${workingDir.path}');
-      print('Exit Code: ${results.exitCode}');
-      print('Output: ${results.stdout}');
-      print('Error: ${results.stderr}');
-      output = results.stdout.toString();
-    });
+    Directory.current = workingDir;
 
-    return output;
+    var shell = Shell();
+
+    await shell
+        .run('sf project retrieve start --metadata CustomObject:Account');
+    // Process.run('ls', ['-l'],
+    //         runInShell: true, workingDirectory: workingDir.path.toString())
+    //     .then((ProcessResult results) {
+    //   print('Current Working Dir: ${workingDir.path}');
+    //   print('Exit Code: ${results.exitCode}');
+    //   print('Output: ${results.stdout}');
+    //   print('Error: ${results.stderr}');
+    //   output = results.stdout.toString();
+    // });
+
+    return "";
   }
 }
