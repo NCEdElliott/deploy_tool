ac82d9afe33b6f5d12ad918451b33f095716a69d
Ed Elliott <ed.elliott@pearson.com>
2023-06-04 21:04:04 -0400

    Latest attempt at spawning a process

--- lib/helpers/shellHelper.dart
+++ lib/helpers/shellHelper.dart
@@ -4,12 +4,13 @@ import 'package:deploy_tool/configuration/appConfig.dart';
 class shellHelper {
   static String retrieveMetadata() {
     String output = "";
-    Directory.current = Directory(AppConfig.getMacOSWorkingDir() + "/TestProject");
+    Directory workingDir = Directory(AppConfig.getMacOSWorkingDir() + "/TestProject");
 
     Process.run('ls', ['-l'], 
                 runInShell: true,
-                workingDirectory: AppConfig.getMacOSWorkingDir()).then((ProcessResult results) {
-      print('Current Working Dir: ' + AppConfig.getMacOSWorkingDir());
+                workingDirectory: workingDir.path.toString())
+            .then((ProcessResult results) {
+      print('Current Working Dir: ' + workingDir.path.toString());
       print('Exit Code: ' + results.exitCode.toString());
       print('Output: ' + results.stdout.toString());
       print('Error: ' + results.stderr.toString());
