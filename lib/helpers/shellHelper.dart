73fb34ffcf5dae6529535af81a9de9e8f52adbca
Ed Elliott <ed.elliott@pearson.com>
2023-06-04 21:15:29 -0400

    Ran dart fix --apply

--- lib/helpers/shellHelper.dart
+++ lib/helpers/shellHelper.dart
@@ -4,16 +4,16 @@ import 'package:deploy_tool/configuration/appConfig.dart';
 class shellHelper {
   static String retrieveMetadata() {
     String output = "";
-    Directory workingDir = Directory(AppConfig.getMacOSWorkingDir() + "/TestProject");
+    Directory workingDir = Directory("${AppConfig.getMacOSWorkingDir()}/TestProject");
 
     Process.run('ls', ['-l'], 
                 runInShell: true,
                 workingDirectory: workingDir.path.toString())
             .then((ProcessResult results) {
-      print('Current Working Dir: ' + workingDir.path.toString());
-      print('Exit Code: ' + results.exitCode.toString());
-      print('Output: ' + results.stdout.toString());
-      print('Error: ' + results.stderr.toString());
+      print('Current Working Dir: ${workingDir.path}');
+      print('Exit Code: ${results.exitCode}');
+      print('Output: ${results.stdout}');
+      print('Error: ${results.stderr}');
       output = results.stdout.toString();
     });
 
